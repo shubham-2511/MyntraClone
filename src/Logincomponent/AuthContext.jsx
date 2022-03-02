687398035b6675a585b377338835f6e27ad35fe4
shreevalikushe <shreevalikushe@gmail.com>
2022-03-02 23:33:03 +0530

    minor changes

--- src/Logincomponent/AuthContext.jsx
+++ src/Logincomponent/AuthContext.jsx
@@ -1,13 +1,11 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-    const [mobileNo, setMobileNo] = useState("")
-    const [createOtp, setCreateOtp] = useState("")
-    const value = { mobileNo, setMobileNo }
-    setCreateOtp(Math.random(4))
-    return (
-        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
-    )
-}
+  const [mobileNo, setMobileNo] = useState("");
+  const [createOtp, setCreateOtp] = useState("");
+  const value = { mobileNo, setMobileNo };
+  // setCreateOtp(Math.random(4))
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,18 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import React from 'react'
-import { Home } from '../HomePageComponents/Home';
-import { ProductCard } from '../components/ProductCard';
+import React from "react";
+import { Home } from "../HomePageComponents/Home";
+import { ProductCard } from "../components/ProductCard";
+import { Login } from "../Logincomponent/Login";
+import { Otp } from "../Logincomponent/Otp";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={ <Home /> } />
-      {/* <Route path='/login' element={<Login />} />
-      <Route path='/otp' element={<Otp />} /> */}
-      <Route path='/product' element={ <ProductCard /> } />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/otp" element={<Otp />} />
+      <Route path="/product" element={<ProductCard />} />
     </Routes>
-  )
-}
+  );
+};
 
 export default AllRoutes;
